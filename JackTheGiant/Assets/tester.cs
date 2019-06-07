5d2f034ab37ae3eead445fc8743db31f1a8d5da0
alexk <alexk@LAPTOP-518CEKNS>
2019-06-06 20:52:25 -0700

    added collider to cloud_1

--- JackTheGiant/Assets/tester.cs
+++ JackTheGiant/Assets/tester.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 public class tester : MonoBehaviour
 {
+    
+    public class PlayerRemote
+    {
+        private GameObject player;
+        private Rigidbody2D rigidbody2D;
+        public float speed = 5;
+        public float jumpPower = 5;
+        public PlayerRemote() { player = null; rigidbody2D = null; }
+        public PlayerRemote(GameObject x)
+        {
+            player = x;
+            rigidbody2D = (Rigidbody2D)player.GetComponent(typeof(Rigidbody2D));
+        }
+
+        public void Mover()
+        {
+            MoverHelperWalk(KeyCode.A, -1);
+            MoverHelperWalk (KeyCode.D, 1);
+            MoverHelperJump(KeyCode.W);
+        }
+        private void MoverHelperWalk(KeyCode kc, int sign)
+        {
+            if (Input.GetKey(kc))
+            {
+                float delta = Time.deltaTime * speed * sign;
+                Vector2 pos = player.transform.position;
+                pos.x += delta;
+                player.transform.position = pos;
+            }
+        }
+        private void MoverHelperJump(KeyCode kc) {
+            if(Input.GetKeyDown(kc))
+               rigidbody2D.AddForce(new Vector2(0,jumpPower));
+        }
+    }
+    public PlayerRemote playerRemote;
     public Sprite sprite;
     public GameObject thePlayer;
     public InputField input;
@@ -12,17 +48,17 @@ public class tester : MonoBehaviour
     void Start()
     {
         animator = (Animator)thePlayer.GetComponent(typeof(Animator));
-
+        playerRemote = new PlayerRemote(thePlayer);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        playerRemote.Mover();
     }
 
     public void PushButton() {
-        ToggleWalk();
+        Adjuster();
     }
     private void CheckForComponent() {
         string name = input.text;
@@ -42,8 +78,14 @@ public class tester : MonoBehaviour
         var x = animator.GetBool(name);
         animator.SetBool(name, !x);
     }
-    private void SpriteTest() {
-       
-       }
+    private void Adjuster() {
+        string data = input.text;
+        var b = int.TryParse(data, out int v);
+        if (b)
+        {
+            playerRemote.speed = v;
+            playerRemote.jumpPower = v;
+        }
+    }
     
 }
