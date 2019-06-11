8bdfc0b4b3e4b2a57df9410fa9bb3f549776f01f
alexk <alexk@LAPTOP-518CEKNS>
2019-06-11 12:44:45 -0700

    removed Background scaler-script from background container-object and put it in each of the background 'panels'

--- JackTheGiant/Assets/BackGroundScaler.cs
+++ JackTheGiant/Assets/BackGroundScaler.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BackGroundScaler : MonoBehaviour
 {
+    public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TEST(string input) {
+
+        try
+        {
+            var item = GetComponent(input);
+            Debug.Log(string.Format("{0}-found : {1}",input,item!=null));
+        }
+        catch {
+            Debug.Log("exception-thrown");
+        }
     }
 }
---
+++ JackTheGiant/Assets/Tester00.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Tester00 : MonoBehaviour
+{
+    public GameObject someObject;
+    public string methodName;
+    public string error_message = "could not send-message";
+    private InputField input;
+    public void Start() {
+        input = (InputField)gameObject.GetComponentInChildren(typeof(InputField));
+    }
+    public void ButtonPress() {
+        try
+        {
+            string inp = input.text;
+            someObject.SendMessage(methodName,inp);
+        }
+        catch
+        {
+            Debug.Log(error_message);
+        }
+    }
+}
