a0e304b6aff5fd056ed89d1008ab5db19233e80b
alexk <alexk@LAPTOP-518CEKNS>
2019-06-13 12:28:32 -0700

    added scripts for cloud-collector and cloud-spawner (and for cloud-manager)

---
+++ JackTheGiant/Assets/CloudManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    private CloudCollector cloudCollector;
+    private CloudSpawner cloudSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloudSpawner = GetComponentInChildren<CloudSpawner>();
+        cloudCollector = GetComponentInChildren<CloudCollector>();
+        int c = 0;
+        if (cloudCollector != null) c++;
+        if (cloudSpawner != null) c += 2;
+        Debug.Log("s/c code == " + c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- JackTheGiant/Assets/Tester00.cs
+++ JackTheGiant/Assets/Tester00.cs
@@ -6,10 +6,30 @@ public class Tester00 : MonoBehaviour
 {
    
     private InputField input;
+    public GameObject cloud;
+    private Stack<GameObject> stack;
     public void Start() {
         input = (InputField)gameObject.GetComponentInChildren(typeof(InputField));
+
+        stack = new Stack<GameObject>();
     }
     public void ButtonPress() {
         Debug.Log("button-pressed");
+        GameObject obj0;
+        switch (input.text)
+        {
+            case "del":
+                if (stack.Count != 0)
+                {
+                    obj0 = stack.Pop();
+                    Destroy(obj0);
+                }
+                break;
+            default:
+                obj0 = Instantiate(cloud);
+                obj0.transform.position = new Vector2(0,0);
+                stack.Push(obj0);
+                break;
+        }
     }
 }
