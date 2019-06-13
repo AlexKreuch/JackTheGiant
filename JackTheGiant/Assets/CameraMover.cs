b04b7df4db7cb2ba305598d7b82861febd6e391b
alexk <alexk@LAPTOP-518CEKNS>
2019-06-13 15:54:11 -0700

    added camera-mover script

---
+++ JackTheGiant/Assets/CameraMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float speed = .1f;
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 vector = transform.position;
+        vector.y -= speed;
+        transform.position = vector;
+    }
+}
