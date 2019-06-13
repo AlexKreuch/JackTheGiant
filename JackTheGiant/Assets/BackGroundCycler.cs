300b527ba5c46247aa313bb71c664b4739ea4604
alexk <alexk@LAPTOP-518CEKNS>
2019-06-13 16:16:50 -0700

    tried adding a script to cycle the backgrounds

---
+++ JackTheGiant/Assets/BackGroundCycler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundCycler : MonoBehaviour
+{
+    private float deltaY = 40;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("COLLISION");
+        Vector2 vector = other.transform.position;
+        vector.y -= deltaY;
+        other.transform.position = vector;
+    }
+
+}
