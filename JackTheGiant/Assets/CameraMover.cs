e713c5d4b85be559f4b55f536306a1308cb1cc6d
alexk <alexk@LAPTOP-518CEKNS>
2019-06-18 21:11:30 -0700

    fixed camera-speed by using a simpler solution

--- JackTheGiant/Assets/CameraMover.cs
+++ JackTheGiant/Assets/CameraMover.cs
@@ -4,36 +4,17 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
+    public float currentSpeed = 0f;
     public float maxSpeed = 5f;
     public float speedUpFactor = 1f;
-    private float distanceTracker = 0f;
-    private float timeTracker = 0f;
     
     private void moveCam() {
-        /*
-         pos(x) = x arctan(x) - (1/2)log(x^2+1) + C
-vel(t) = c0 * arctan(c2*x+c3)
-
-\int arctan(x) dx = x arctan(x) - (1/2)log(x^2+1) + C
-
-let F(t) := x arctan(x) - (1/2)log(x^2+1) 
- ( note then : F'(t) = arctan(x) )
-now let G[a,b](t) := a*F(b*t)
- ( note then : G'(t) = ab*F'(bt) = ab*arctan(bt) )
-note that G'(t) approches ab*(\pi/2)
-
-so : set a=(max/b)/(pi/2)
-         */
-        float b = speedUpFactor!=0 ? speedUpFactor : 1f;
-        float a = (maxSpeed / b) * .5f / Mathf.PI;
-        float t = timeTracker + Time.deltaTime;
-        float cur_d = t * Mathf.Atan(t) - .5f * Mathf.Log(t*t+ 1);
-        float delta_d = cur_d - distanceTracker;
-        distanceTracker = cur_d;
-        timeTracker = t;
+        float delta_y = currentSpeed * Time.deltaTime;
+        currentSpeed += speedUpFactor * Time.deltaTime;
+        if (currentSpeed > maxSpeed) currentSpeed = maxSpeed;
 
         Vector2 vector = transform.position;
-        vector.y -= delta_d;
+        vector.y -= delta_y;
         transform.position = vector;
     }
     void Update() { moveCam(); }
--- JackTheGiant/Assets/Tester00.cs
+++ JackTheGiant/Assets/Tester00.cs
@@ -10,17 +10,21 @@ public class Tester00 : MonoBehaviour
     private Vector2[] campos;
     private int cur = 0;
     private float dt = 1f;
+    private bool adjusting = false;
     private void setup_vals() {
         campos = new Vector2[2];
         campos[0] = Camera.main.transform.position;
         campos[1] = Camera.main.transform.position;
     }
     private void update_vals() {
+        adjusting = true;
         campos[cur] = Camera.main.transform.position;
         cur = (cur + 1) % 2;
         dt = Time.deltaTime;
+        adjusting = false;
     }
     private float computeSpeed() {
+        if (adjusting) Debug.Log("speed = ADJUSTING");
         Vector2 vector = campos[0] - campos[1];
         float distance = vector.magnitude;
         return distance/dt;
