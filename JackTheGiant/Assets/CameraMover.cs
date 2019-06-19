bdbbbef06d95a7463f79f3f22b2fc88bf7cb00ca
alexk <alexk@LAPTOP-518CEKNS>
2019-06-18 19:46:06 -0700

    tried to implement gradual exeleration for the camera, but something went wrong

--- JackTheGiant/Assets/CameraMover.cs
+++ JackTheGiant/Assets/CameraMover.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
+    public float grav = .1f;
+    public float drag = .5f;
+    private float timeTracker = 0f;
+    private float computePath(float t) {
+        /*
+p(t) = -a*log(b*cos(cx))
+p'(t) = ( -a/b*cos(cx) ) * ( -b*sin(cx) * c )
+      = ( ac/cos(cx) ) * ( sin(cx) )
+      = (ac) * tan(cx)
+p''(t) = (ac) * ( 1 + tan^2(cx) ) * c
+       =  ac^2 + ac^2tan^2(cx) 
+       =  ac^2 + a^2c^2tan^2(cx) / a
+       =  ac^2 + (ac*tan(cx))^2 / a
+       =  ac^2 + (1/a)*( p'(t) )^2
+A = ac^2 ; B = 1/a
+a = 1/B ; c = \sqrt{AB}
+*/
+        float a = drag == 0 ? (float)1E10 : 1 / drag;
+        float c = Mathf.Sqrt(Mathf.Abs(drag*grav));
+        var intermed = Mathf.Cos(c * t);
+        Debug.Log("Mathf.Cos(c * t) = " + intermed);
+        var res = -1 * a * Mathf.Log(Mathf.Cos(c * t));
+        return res;
+    }
+    private void moveCam() {
+        float y0 = computePath(timeTracker);
+        timeTracker += Time.deltaTime;
+        float y1 = computePath(timeTracker);
+        Debug.Log("aa | " + y1);
+        float deltay = y0 - y1;
+        Vector2 vector = transform.position;
+        vector.y += deltay;
+        transform.position = vector;
+    }
+    void Update() { moveCam(); }
+    /*
+    // OLD :
+
     public float speed = .1f;
-   
+    
 
     // Update is called once per frame
     void FixedUpdate()
@@ -14,4 +52,5 @@ public class CameraMover : MonoBehaviour
         vector.y -= speed;
         transform.position = vector;
     }
+    */
 }
--- JackTheGiant/Assets/Tester00.cs
+++ JackTheGiant/Assets/Tester00.cs
@@ -4,131 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Tester00 : MonoBehaviour
 {
-   
+
     private InputField input;
 
-    
-    
+
+
     public void Start() {
         input = (InputField)gameObject.GetComponentInChildren(typeof(InputField));
 
-        
+
     }
     public void ButtonPress() {
         string x = input.text;
-        string y = Compute(x);
-        y = "<~~     ~~>" + y;
+        string y = "<   >" + Comp1(x);
         Debug.Log(y);
     }
-
-    struct GenType {
-       public readonly char tcode;
-       public readonly object data;
-       private bool hasMess;
-       private string mess;
-       public GenType(char t, object d) { tcode = t; data = d; hasMess = false; mess = ""; }
-        public GenType(char t, object d, string m)
+    private int[] Comp0(string inp) {
+        int[] res = new int[] { 0 , 0 , 0 , 0 };
+        string[] tokens = inp.Split(',');
+        if (tokens.Length != 3) { return res; }
+        for (int i = 0; i < 3; i++)
         {
-            tcode = t; data = d; hasMess = true; mess = m;
+            bool flg = int.TryParse(tokens[i].Trim() , out res[i+1]);
+            if (!flg) return res;
         }
-       public string Q(string q) {
-            switch (q)
-            {
-                case "has_mess": return hasMess ? "yes" : "no";
-                case "get_mess": return mess;
-                default: return "";
-            }
-        }
+        res[0] = 1;
+        return res;
     }
-    GenType MyTryParse(string inp)
+    private string Comp1(string inp)
     {
-        if (inp.Length == 0) return new GenType('_',null);
-        char tp = inp[0];
-        inp = inp.Substring(1);
-        bool flg = false; int i0 = 0; float f0 = 0f;
-        object obj = null;
-        switch (tp)
-        {
-            case 'i':
-                flg = int.TryParse(inp,out i0);
-                if (flg) obj = i0;
-                break;
-            case 'f':
-                flg = float.TryParse(inp,out f0);
-                if (flg) obj = f0;
-                break;
-        }
-        char code = flg ? tp : '_';
-        return new GenType(code,obj);
-    }
-    GenType TryRandRange(char tp, GenType g1, GenType g2) {
-        int senarioCode(char a, GenType b, GenType c) {
-            char[] co = new char[] { a, b.tcode, c.tcode};
-            int r = 0, p = 1;
-            for (int i = 0; i < 3; i++)
-            {
-                switch (co[i])
-                {
-                    case 'i': break;
-                    case 'f': r += p; break;
-                    default: return -1;
-                }
-                p *= 2;
-            }
-            return r;
-        }
-        string makeMessage(object a, GenType b, GenType c, string d)
-        {
-            if (d.Length != 3) return "<ERROR>";
-            return string.Format("{0}{3}~=RR({1}{4}, {2}{5})",a,b.data,c.data,d[0],d[1],d[2]);
-        }
-        int sc = senarioCode(tp,g1,g2);
-        int i0 = 0; float f0 = 0f;
-        char t0 = '_'; object obj = null;
-        string m0 = "";
-        switch (sc)
-        {
-            case -1: return new GenType('_',null,"invalid input");
-            case 0:
-                i0 = Random.Range((int)g1.data,(int)g2.data);
-                obj = i0; t0 = 'i'; m0 = makeMessage(obj,g1,g2,"iii");
-                break;
-            case 1:
-                f0 = Random.Range((int)g1.data, (int)g2.data);
-                obj = f0; t0 = 'f'; m0 = makeMessage(obj, g1, g2, "fii");
-                break;
-            case 2: return new GenType(t0,obj,"i=RR(f,i) ==> invalid");
-            case 3: 
-                f0 = Random.Range((float)g1.data, (int)g2.data);
-                obj = f0; t0 = 'f'; m0 = makeMessage(obj, g1, g2, "ffi");
-                break;
-            case 4: return new GenType(t0, obj, "i=RR(i,f) ==> invalid");
-            case 5:
-                f0 = Random.Range((int)g1.data, (float)g2.data);
-                obj = f0; t0 = 'f'; m0 = makeMessage(obj, g1, g2, "fif");
-                break;
-            case 6: return new GenType(t0, obj, "i=RR(f,f) ==> invalid");
-            case 7:
-                f0 = Random.Range((float)g1.data, (float)g2.data);
-                obj = f0; t0 = 'f'; m0 = makeMessage(obj, g1, g2, "fff");
-                break;
-            default: return new GenType(t0, obj, "invalid senario-code");
-        }
+        int[] data = Comp0(inp);
+        if (data[0] == 0) return "INVALID";
+        int x = Mathf.Clamp(data[1], data[2], data[3]);
 
-        return new GenType(t0,obj,m0);
-    }
-    string Compute(string inp)
-    {
-        string[] tokens = inp.Split('|');
-        if (tokens.Length != 3) return "<INVALID>-[bad-length]";
-        if(tokens[0]!="i" && tokens[0]!="f") return "<INVALID>-[bad-start]";
-        GenType g1 = MyTryParse(tokens[1]);
-        if(g1.tcode=='_') return "<INVALID>-[1st-term]";
-        GenType g2 = MyTryParse(tokens[2]);
-        if (g2.tcode == '_') return "<INVALID>-[2nd-term]";
-        GenType g3 = TryRandRange(tokens[0][0],g1,g2);
-        if (g3.tcode == '_') return "oops | " + g3.Q("get_mess");
-        return "success | " + g3.Q("get_mess");
+        return string.Format("Mathf.Clamp({0},{1},{2}) = {3}" , data[1] , data[2] , data[3] , x);
     }
 }
  
