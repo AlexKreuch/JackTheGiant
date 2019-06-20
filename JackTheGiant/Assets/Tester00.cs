a6a3ede3ac26977e7219eb66a4be59c9a5198415
alexk <alexk@LAPTOP-518CEKNS>
2019-06-19 18:00:09 -0700

    adjusted scale

--- JackTheGiant/Assets/Tester00.cs
+++ JackTheGiant/Assets/Tester00.cs
@@ -6,39 +6,44 @@ public class Tester00 : MonoBehaviour
 {
 
     private InputField input;
-
-    private Vector2[] campos;
-    private int cur = 0;
-    private float dt = 1f;
-    private bool adjusting = false;
-    private void setup_vals() {
-        campos = new Vector2[2];
-        campos[0] = Camera.main.transform.position;
-        campos[1] = Camera.main.transform.position;
-    }
-    private void update_vals() {
-        adjusting = true;
-        campos[cur] = Camera.main.transform.position;
-        cur = (cur + 1) % 2;
-        dt = Time.deltaTime;
-        adjusting = false;
-    }
-    private float computeSpeed() {
-        if (adjusting) Debug.Log("speed = ADJUSTING");
-        Vector2 vector = campos[0] - campos[1];
-        float distance = vector.magnitude;
-        return distance/dt;
-    }
+    private GameObject player;
+    private Rigidbody2D rigidbody;
+    private Vector2 init_pos;
+    public bool firstUpdate = true;
+    public float scale = 1f;
     public void Start() {
         input = (InputField)gameObject.GetComponentInChildren(typeof(InputField));
 
-        setup_vals();
+        player = GameObject.FindGameObjectWithTag("Player");
+        rigidbody = player.GetComponent<Rigidbody2D>();
+        init_pos = player.transform.position;
     }
-    public void Update() { update_vals(); }
+    public void Update() {
+        if (firstUpdate)
+        {
+            init_pos = player.transform.position;
+            firstUpdate = false;
+        }
+        if (player.transform.localScale.x != scale)
+        {
+            Vector2 vector = player.transform.localScale;
+            vector.x = scale;
+            vector.y = scale;
+            player.transform.localScale = vector;
+        }
+    }
     public void ButtonPress() {
-        var v = computeSpeed();
-        Debug.Log("speed = " + v);
+        string inp = input.text;
+        if (inp == "reset")
+        {
+            player.transform.position = init_pos;
+            rigidbody.velocity = new Vector2(0f,0f);
+            return;
+        }
+        
     }
+
+    
     
 }
  
