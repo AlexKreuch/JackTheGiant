fa0dd69aefa42b19f8d85c4048ee063ec3d9df62
alexk <alexk@LAPTOP-518CEKNS>
2019-06-20 19:01:19 -0700

    more testing ; moved buttons

--- JackTheGiant/Assets/MenuTester00.cs
+++ JackTheGiant/Assets/MenuTester00.cs
@@ -32,14 +32,31 @@ public class MenuTester00 : MonoBehaviour
             sudoButton = false;
             push_sudo_button();
         }
+        maintain_scale();
+    }
+    private RectTransform RT;
+    private void maintain_scale() {
+        if (RT == null)
+        {
+            RT = GetComponent<RectTransform>();
+        }
+        Vector2 vector = RT.localScale;
+        if (vector.x != vector.y)
+        {
+            vector.x = vector.y;
+            RT.localScale = vector;
+        }
     }
     private void push_sudo_button() {
-        switch (sudoInput) {
+        string[] tokens = Custom_helper0(sudoInput,'_');
+
+        switch (tokens[0]) {
             case "": break;
             case "chkComp": helper0(); break;
             case "chkRect": helper1(); break;
             case "tryReset": helper2();  break;
             case "tryReset-c": helper3(); break;
+            case "custom": Custom(tokens[1]); break;
             default: Debug.Log("not-recognized"); break;
         }
     }
@@ -57,5 +74,35 @@ public class MenuTester00 : MonoBehaviour
     private void helper3() {
         var rt = ResetScale();
         Debug.Log("ran ResetScale ; new rect should be : " + rt.rect);
+    }
+
+    private void Custom(string inp) {
+        void tool(string code, out bool itWorked, out RectTransform.Axis ax, out float v) {
+            ax = RectTransform.Axis.Horizontal;
+            v = 0f;
+            itWorked = false;
+            if (code.Length < 2) return;
+            switch (code[0])
+            {
+                case 'h': break;
+                case 'v': ax = RectTransform.Axis.Vertical; break;
+                default: return;
+            }
+            itWorked = float.TryParse(code.Substring(1),out v);
+
+        }
+        var r = GetComponent<RectTransform>();
+        tool(inp, out bool flg, out RectTransform.Axis axis, out float val);
+        if (!flg) { Debug.Log("oops"); return; }
+        r.SetSizeWithCurrentAnchors(axis,val);
+        Debug.Log("did-thing");
+    }
+    private string[] Custom_helper0(string inp, char div) {
+        string[] tokens = inp.Split(div);
+        if (tokens.Length == 0) return new string[] { "", "" };
+        if (tokens.Length == 1) return new string[] { tokens[0], "" };
+        string s = tokens[1];
+        for (int i = 2; i < tokens.Length; i++) s += div + tokens[i];
+        return new string[] { tokens[0] , s };
     }
 }
