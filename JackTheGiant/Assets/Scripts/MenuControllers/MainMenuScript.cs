73d2683076cb88eb65479f40f3474bfb3f8a9e9d
alexk <alexk@LAPTOP-518CEKNS>
2019-06-24 11:55:22 -0700

    added read-me file

--- JackTheGiant/Assets/Scripts/MenuControllers/MainMenuScript.cs
+++ JackTheGiant/Assets/Scripts/MenuControllers/MainMenuScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region scene-names
+    private const string HighScore_sceneName = "HighScore00";
+    private const string MainMenu_sceneName = "MainMenu00";
+    private const string OptionsMenu_sceneName = "OptionsMenu";
+    private const string GamePlay_sceneName = "Scene00";
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
+    #region button-methods
+
+    public void StartGame() {
         
     }
+    public void Options() { }
+    public void HighScore() { }
+    public void Quit() { }
+    public void Music() { }
+
+    #endregion
+    /**
+buttons : 
+start-game
+options
+high-score
+quit
+toggle-music
+**/
 }
---
+++ README.txt
@@ -0,0 +1,4 @@
+<note date=06/24/2019>
+In the video-tutorials, they use Application.loadLevel, but this method
+is depreciated.
+</note>
