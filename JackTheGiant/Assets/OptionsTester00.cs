7bf582389177c6c1d5c263491983a40a5fbb58a4
alexk <alexk@LAPTOP-518CEKNS>
2019-06-24 10:21:20 -0700

    in Options-menu scene : added background image, and adjusted scale to (W=465, H=559)

---
+++ JackTheGiant/Assets/OptionsTester00.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class OptionsTester00 : MonoBehaviour
+{
+    public UnityEngine.UI.Image image;
+    private bool notStarted = true;
+    private float x_to_y = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Adjust();
+    }
+
+    private void Adjust() {
+        float safeDiv(float x, float y) { if (y == 0) y = .001f; return x / y; }
+        if (image == null) return;
+        if (notStarted)
+        {
+            x_to_y = safeDiv(image.rectTransform.sizeDelta.x , image.rectTransform.sizeDelta.y);
+            notStarted = false;
+        }
+        Vector2 vector = image.rectTransform.sizeDelta;
+        if (safeDiv(vector.x, vector.y) != x_to_y)
+        {
+            vector.y = safeDiv(vector.x,x_to_y);
+            image.rectTransform.sizeDelta = vector;
+        }
+    }
+    
+}
