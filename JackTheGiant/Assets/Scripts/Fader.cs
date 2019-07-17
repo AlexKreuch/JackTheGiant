c7e0afb954747c9f2232b52f923d65271a1980a3
alexk <alexk@LAPTOP-518CEKNS>
2019-07-16 20:31:46 -0700

    trying too add 'fading' animation ; got fader set-up

--- JackTheGiant/Assets/Scripts/Fader.cs
+++ JackTheGiant/Assets/Scripts/Fader.cs
@@ -1,41 +1,117 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Fader : MonoBehaviour
 {
     public static Fader instance;
 
+    public float Opacity = 0f;
+
+    private const string triggerName = "StartFadeCycle";
+    private Animator animator;
+    private GameObject panel;
+    private Image image;
+    private enum PAS { FADEIN , FADEOUT , IDLE , STARTING }; // PAS := Predicted Animator State
+    private PAS pas = PAS.STARTING;
+
+
+    private void MaintainOpacity() {
+        if (panel.activeSelf)
+        {
+            var c = image.color;
+            c.a = Opacity;
+            image.color = c;
+        }
+    }
+    
     void Awake() {
         if (instance == null)
         {
             instance = this;
+            animator = GetComponent<Animator>();
+            image = GetComponentInChildren<Image>();
+            panel = image.gameObject;
             DontDestroyOnLoad(gameObject);
         }
         else
             Destroy(gameObject);
     }
 
-    void Start() {
-        Animator animator = GetComponentInChildren<Animator>();
-        bool b = ReportStatus(animator,"theAnimator");
-        if (b)
+   
+
+    void Update() {
+        MaintainOpacity();
+        btn_test();
+    }
+
+    public void FadeToNextScene(string nextScene) {
+        Debug.Assert(pas==PAS.IDLE,"tried to fade scenes durring fade");
+        QuedScene = nextScene;
+        StartFading();
+    }
+
+    public void FadingEvent(int x) {
+        int c = -1;
+        switch (pas)
         {
-            GameObject obj = animator.gameObject;
-            ReportStatus(obj,"animatorHost");
+            case PAS.FADEIN: c = 0; break;
+            case PAS.FADEOUT: c = 1; break;
+            case PAS.IDLE: c = 2; break;
+            case PAS.STARTING: c = 3; break;
         }
-
+        c = (2 * c) + x;
+        switch (c)
+        {
+            case 6: // pas==STARTING, x==0
+                pas = PAS.FADEIN;
+                break;
+            case 1: // pas==FADEIN, x==1
+                pas = PAS.IDLE;
+                panel.SetActive(false);
+                break;
+            case 5: // pas==IDLE, x==1
+                animator.speed = 1f;
+                pas = PAS.FADEOUT;
+                panel.SetActive(true);
+                break;
+            case 2: // pas==FADEOUT, x==0
+                pas = PAS.FADEIN;
+                gotoNextScene();
+                break;
+            case 0: // pas==FADEIN, x==0
+                break;
+            default:
+                Debug.Assert(false,"INVALID FADER-STATE : c==" + c);
+                return;
+        }
     }
 
-    private bool ReportStatus(Object obj, string title) {
-        bool res = obj != null;
-        if (res) Debug.Log(string.Format("{0} found | name == {1}", title, obj.name));
-        else Debug.Log(title + " not-found");
-        return res;
+    private void StartFading() {
+        if (pas != PAS.IDLE) return;
+        animator.speed = 100f;
+        animator.SetTrigger(triggerName);
     }
 
-    private const string triggerName = "StartFadeCycle";
 
-  
+    private string QuedScene = "";
+
+    private void gotoNextScene() {
+        if (QuedScene == "") return;
+        SceneManager.LoadScene(QuedScene);
+    }
+
+    public bool btn = false;
+    private void btn_test() {
+        if (btn)
+        {
+            btn = false;
+            string nm = "MainMenu00";
+            FadeToNextScene(nm);
+        }
+    }
+    
+    
 }
