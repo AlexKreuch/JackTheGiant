a0e1b7fff71e6b4f3e4e41b20dea8e065d716ee3
alexk <alexk@LAPTOP-518CEKNS>
2019-07-18 16:21:58 -0700

    trying too add 'joy_stick

---
+++ JackTheGiant/Assets/JoyStick.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoyStick : MonoBehaviour
+{
+    private const string Rtag = "Right_button";
+    private const string Ltag = "Left_button";
+
+    private UnityEngine.UI.Button Lbtn, Rbtn;
+
+    private void GetButtons() {
+        Button[] buttons = GetComponentsInChildren<Button>();
+        int i = buttons[0].tag == Ltag ? 0 : 1;
+        Lbtn = buttons[i];
+        Rbtn = buttons[(i + 1) % 2];
+    }
+    private void ScaleButtons() {
+      
+        Sprite sprite = Lbtn.GetComponent<Image>().sprite;
+        
+
+        float camH = Camera.main.orthographicSize * 2;
+        float camW = camH * (Screen.width / Screen.height) * Camera.main.rect.width;
+
+        float btnH = sprite.bounds.size.y;
+        float btnW = sprite.bounds.size.x;
+
+        float xfactor = (camW / btnW) / 2;
+        float yfactor = camH / btnH;
+
+        float GET_x_pos(int asdf_0)
+        {
+            float asdf_1 = camW / 4;
+            if (asdf_0 == 0) asdf_1 *= -1;
+            return asdf_1;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            Button button = i == 0 ? Lbtn : Rbtn;
+            Vector2 pos = button.transform.localPosition;
+            Vector2 sca = button.transform.localScale;
+
+            pos.x = GET_x_pos(i);
+            sca.x = xfactor;
+            sca.y = yfactor;
+
+            button.transform.localPosition = pos;
+            button.transform.localScale = sca;
+        }
+
+
+    }
+
+    void Awake() {
+        GetButtons();
+        ScaleButtons();
+    }
+}
