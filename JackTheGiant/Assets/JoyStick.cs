ed43b00432f50262f66c02a0ec5eb5ed667e4325
alexk <alexk@LAPTOP-518CEKNS>
2019-07-21 16:49:15 -0700

    trying too add 'joy_stick' ; did testing

--- JackTheGiant/Assets/JoyStick.cs
+++ JackTheGiant/Assets/JoyStick.cs
@@ -16,46 +16,15 @@ public class JoyStick : MonoBehaviour
         Lbtn = buttons[i];
         Rbtn = buttons[(i + 1) % 2];
     }
-    private void ScaleButtons() {
-      
-        Sprite sprite = Lbtn.GetComponent<Image>().sprite;
-        
+  
 
-        float camH = Camera.main.orthographicSize * 2;
-        float camW = camH * (Screen.width / Screen.height) * Camera.main.rect.width;
-
-        float btnH = sprite.bounds.size.y;
-        float btnW = sprite.bounds.size.x;
-
-        float xfactor = (camW / btnW) / 2;
-        float yfactor = camH / btnH;
-
-        float GET_x_pos(int asdf_0)
-        {
-            float asdf_1 = camW / 4;
-            if (asdf_0 == 0) asdf_1 *= -1;
-            return asdf_1;
-        }
-
-        for (int i = 0; i < 2; i++)
-        {
-            Button button = i == 0 ? Lbtn : Rbtn;
-            Vector2 pos = button.transform.localPosition;
-            Vector2 sca = button.transform.localScale;
-
-            pos.x = GET_x_pos(i);
-            sca.x = xfactor;
-            sca.y = yfactor;
-
-            button.transform.localPosition = pos;
-            button.transform.localScale = sca;
-        }
-
-
-    }
+   
 
     void Awake() {
         GetButtons();
-        ScaleButtons();
+       
+      
     }
+
+  
 }
--- JackTheGiant/Assets/Scene00_tester00.cs
+++ JackTheGiant/Assets/Scene00_tester00.cs
@@ -1,36 +1,132 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 [ExecuteAlways]
 public class Scene00_tester00 : MonoBehaviour
 {
-    [SerializeField]
-    public RectTransform rt;
 
-    public bool btn0 = false;
-    public bool btn1 = false;
+    public Vector3 buttonPos = new Vector3();
+    private class GetButton {
+        private const string btnName = "Left_button";
+        private static Button lftButton = null;
+        private static RectTransform rectTransform = null;
+        public static Button Get() {
+            if (lftButton == null)
+            {
+                var obs = FindObjectsOfType<Button>();
+                foreach (Button x in obs) if (x.tag == btnName)
+                    {
+                        lftButton = x;
+                        break;
+                    }
 
-    private void scale(float x) {
-        Vector2 vector = rt.sizeDelta;
-        vector *= x;
-
-        rt.sizeDelta = vector;
-
+            }
+            return lftButton;
+        }
+        public static RectTransform GetRT() {
+            if (rectTransform == null)
+            {
+                var button = Get();
+                rectTransform = button.GetComponent<RectTransform>();
+            }
+            return rectTransform;
+        }
     }
-
-    private void Check(float x, ref bool btn)
-    {
-        if (btn)
-        {
-            scale(x);
-            btn = false;
+    private class GetCanvas{
+        private static Canvas canvas = null;
+        public static Canvas Get() {
+            if (canvas == null)
+            {
+                var js = FindObjectOfType<JoyStick>();
+                canvas = js.GetComponentInChildren<Canvas>();
+            }
+            return canvas;
         }
 
     }
-    
+
+    public bool btn = false;
+    private void pressButton() {
+        float hh = Camera.main.orthographicSize / GetCanvas.Get().transform.localScale.x;
+        Debug.Log("scaled-val == " + hh);
+    }
+
+    private string ftos(float input, int n = 10) {
+        void split(float x, out int y, out float z)
+        {
+            y = Mathf.FloorToInt(x);
+            z = x - y;
+        }
+        char compDigit(int x) {
+            return ((char)(((int)('0')) + x));
+        }
+        if (input == 0) return "0";
+        bool isNegative = input < 0;
+        if (isNegative) input *= -1;
+        split(input, out int intPart, out float dec);
+        string res = intPart.ToString();
+        if (isNegative) res = '-' + res;
+        if (dec == 0f) return res;
+        res += '.';
+        for (int i = 0; i < n; i++)
+        {
+            if (dec == 0f) break;
+            dec *= 10;
+            split(dec, out int dig, out dec);
+            res += compDigit(dig);
+        }
+
+        return res;
+    }
+    private string vtos(Vector3 vector, int n = 10)
+    {
+        string sx = ftos(vector.x, n);
+        string sy = ftos(vector.y, n);
+        string sz = ftos(vector.z, n);
+        return string.Format("( {0}, {1}, {2} )", sx, sy, sz);
+    }
 
     void Update() {
-        Check(2f, ref btn0);
-        Check(.5f, ref btn1);
+        CheckButton(ref btn, pressButton);
+     //   MainTainPos();
+     //   PressChange();
     }
+
+    private void MainTainPos() {
+        var rt = GetButton.GetRT();
+        if (rt.localPosition != buttonPos)
+        {
+            rt.localPosition = buttonPos;
+        }
+    }
+
+    public bool chng = false;
+    public Vector3 inputVec = new Vector3();
+    private void PressChange() {
+        if (chng)
+        {
+            chng = false;
+            Vector3 vector = Camera.main.ScreenToWorldPoint(inputVec, Camera.MonoOrStereoscopicEye.Mono);
+            var rt = GetButton.GetRT();
+            rt.position = vector;
+            buttonPos = rt.localPosition;
+        }
+
+    }
+
+
+
+    private void CheckButton(ref bool b, System.Action action)
+    {
+        if (b)
+        {
+            b = false;
+            action();
+        }
+    }
+
+    
+
 }
